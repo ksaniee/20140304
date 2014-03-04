2925bce5078739fa321150298f64126af89894f0
ksaniee <ksaniee@integralads.com>
2014-03-04 13:58:12 -0500

    Update setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -1,3 +1,9 @@
 startdate=`date -d "-15 day" +"%y%m%d0000"` 
 enddate=`date -d "-1 day" +"%y%m%d0000"` 
 today=`date -d "0 day" +"%y%m%d0000"`
+
+INPUT=hbase://q_pages
+OUTPUT=hdfs://mars/user/top10m$today
+
+
+
