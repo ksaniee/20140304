756d08e21d0d7be333c2d91f284761389df7fc21
ksaniee <ksaniee@integralads.com>
2014-03-04 13:51:42 -0500

    Update job.sh

--- job.sh
+++ job.sh
@@ -1,1 +1,1 @@
-pig -param startdate=`date -d "-15 day" +"%y%m%d0000"` -param enddate=`date -d "-1 day" +"%y%m%d0000"` -param today=`date -d "0 day" +"%y%m%d0000"` crawl10mlist.pig
+pig -param startdate=`date -d "-15 day" +"%y%m%d0000"` -param enddate=`date -d "-1 day" +"%y%m%d0000"` -param today=`date -d "0 day" +"%y%m%d0000"` crl5mlist.pig
